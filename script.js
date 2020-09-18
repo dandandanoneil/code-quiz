f9bdfaff0566926ee0eb0c15fcfd98612215e670
Dan ONeil <dhoneil@gmail.com>
2020-09-18 12:33:08 -0400

    Created index.html for the landing page and quiz, high-scores.html for the high score board, and script.js to contain the JavaScript.

---
+++ script.js
@@ -0,0 +1,12 @@
+// This script will include
+
+// Button actions:
+//   - "Start" button on index.html will
+//     - hide the start message, start button, and high scores button
+//     - show the timer, question/answer card, and result message box
+//     - start the timer and countdown
+//     - start showing the question objects from the questions array
+//   - "Clear Scores" button on high-scores.html will
+//     - clear the array of high score objects
+// Timer
+// Questions array of question objects with right answer and wrong answer keys
