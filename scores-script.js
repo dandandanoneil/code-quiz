8c4540fc199ba32182f87b9f3d139e7fff691dcb
Dan ONeil <dhoneil@gmail.com>
2020-09-20 23:45:03 -0400

    Resturctured javascript to make functions and eventListeners clearer

--- scores-script.js
+++ scores-script.js
@@ -6,51 +6,52 @@ const clearButton = document.querySelector("#clear-button");
 
 // Dummy scores in localStorage  - uncomment for testing
 // localStorage.setItem("scores", JSON.stringify(
-//     [{userName:"Dan1", userScore:"30"}, 
-//     {userName:"Dan2", userScore:"2"}, 
-//     {userName:"Dan3", userScore:"12"}, 
-//     {userName:"Dan4", userScore:"-20"}]));
-
+//    [{userName:"DHO", userScore:"30"}, 
+//     {userName:"ASB", userScore:"2"}, 
+//     {userName:"JPB", userScore:"12"}, 
+//     {userName:"MSD", userScore:"-20"}] ) );
+    
 // Initial code to load high scores
 renderScoreList()
-
+    
 function renderScoreList() {    
     // Get high scores from localStorage and convert to an array
     let highScores = JSON.parse(localStorage.getItem("scores"));
     if(highScores === null) { highScores = []; }
-
+    
     // Clear the scoreList element
     scoreList.innerHTML = "";
-
+    
     // Create list of high scores in scoreList element
     if(highScores === []) { 
         for (let i = 0; i < 10; i++) {
-            let score = document.createElement("div");
-            score.innerText = ((i + 1) + ". ???")
-            scoreList.appendChild(score)
+            let scoreDiv = document.createElement("div");
+            scoreDiv.innerText = ((i + 1) + ". ???")
+            scoreList.appendChild(scoreDiv)
         }
     } else {
         for (let i = 0; i < highScores.length; i++) {
-            let score = document.createElement("div");
-            score.innerText = ((i + 1) + ". " + highScores[i].userName + ": " + highScores[i].userScore)
-            scoreList.appendChild(score)
+            let scoreDiv = document.createElement("div");
+            scoreDiv.innerText = ((i + 1) + ". " + highScores[i].userName + ": " + highScores[i].userScore)
+            scoreList.appendChild(scoreDiv)
         }
         // If there are less than 10 high scores, add empty numbered lines
         if(highScores.length < 10) {
             for (let i = highScores.length; i < 10; i++) {
-                let score = document.createElement("div");
-                score.innerText = ((i + 1) + ". ???")
-                scoreList.appendChild(score)
+                let scoreDiv = document.createElement("div");
+                scoreDiv.innerText = ((i + 1) + ".")
+                scoreList.appendChild(scoreDiv)
             }    
         }
     }
 }
 
-// Clear Button Code
-clearButton.addEventListener("click", function() {
+function clearScores() {
     if(confirm("Are you sure you want to clear all the accumulated high scores?")) {
         console.log("scores cleared");
         localStorage.setItem("scores", JSON.stringify( [] ));
         renderScoreList();
     }
-});
+}
+
+clearButton.addEventListener("click", clearScores);
